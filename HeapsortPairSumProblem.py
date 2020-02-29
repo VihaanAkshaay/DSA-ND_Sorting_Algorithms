502b0b2eb0311683eb1cb2c1e6c4799909aefd78
VihaanAkshaay <akswins001@gmail.com>
2020-02-29 19:33:36 +0530

    Pair Sum Function Added

---
+++ HeapsortPairSumProblem.py
@@ -0,0 +1,25 @@
+'''Given an input array and a target value (integer), find two values in the array whose sum is equal to the target value. Solve the problem without using extra space. You can assume the array has unique values and will never have more than one solution.
+'''
+
+
+def pair_sum(arr, target):
+    # sort the list
+    arr.sort()
+
+    # initialize two pointer - one from start of the array and other from the end
+    front_index = 0
+    back_index = len(arr) - 1
+
+    # shift the pointers
+    while front_index < back_index:
+        front = arr[front_index]
+        back = arr[back_index]
+
+        if front + back == target:
+            return [front, back]
+        elif front + back < target:  # sum < target ==> shift front pointer forward
+            front_index += 1
+        else:
+            back_index -= 1  # sum > target ==> shift back pointer backward
+
+    return [None, None]
