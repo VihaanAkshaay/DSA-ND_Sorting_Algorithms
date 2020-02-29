197527485d0bad347e618813b765f0a823156b9b
VihaanAkshaay <akswins001@gmail.com>
2020-02-29 15:42:59 +0530

    Case specific Sorting of Strings problem added'

--- MergeSortStrings.py
+++ MergeSortStrings.py
@@ -4,27 +4,30 @@ and lowercase letters separately, such that if the 𝑖th place
 in the original string had an uppercase character then it should
  not have a lowercase character after being sorted and vice versa.'''
 
+
 def case_sort(string):
     upper_ch_index = 0
     lower_ch_index = 0
 
     sorted_string = sorted(string)
     for index, character in enumerate(sorted_string):
+        # check if character is lower-case
         ascii_int = ord(character)
-        if 97 <= ascii_int <= 122:
+        if 97 <= ascii_int <= 122:  # ASCII value of a = 97 & ASCII value of z = 122
             lower_ch_index = index
             break
+
     output = list()
-    for characted in string:
+    for character in string:
         ascii_int = ord(character)
+        # if character is lower case pick next lower_case character
         if 97 <= ascii_int <= 122:
             output.append(sorted_string[lower_ch_index])
-            lower_ch_index +=1
-        else:
-            output.append(sorted_string[lower_ch_index])
             lower_ch_index += 1
+        else:
+            output.append(sorted_string[upper_ch_index])
+            upper_ch_index += 1
     return "".join(output)
-
 
 def test_function(test_case):
     test_string = test_case[0]
